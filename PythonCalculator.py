514e971b93823c8b64d36847a98a9cc4ac593040
danielled25 <34135850+danielled25@users.noreply.github.com>
2018-11-19 14:35:42 +0000

    Initial Commit

---
+++ PythonCalculator.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+First Iteration of Simple Python Calculator
+Functionality: Menu and get input from user
+"""
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+# Take input from the user 
+choice = input("Enter choice(1/2/3/4):")
+
